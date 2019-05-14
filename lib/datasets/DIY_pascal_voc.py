d69edd7251dd62a5c122aac394a30cded20344f1
DESKTOP-M7BS054\nlandy <nlandy8@gmail.com>
2019-05-13 21:45:37 -0700

    Adding validation/test script

--- lib/datasets/DIY_pascal_voc.py
+++ lib/datasets/DIY_pascal_voc.py
@@ -163,122 +163,122 @@ class DIY_pascal_voc(imdb):
                 'flipped': False,
                 'seg_areas': seg_areas}
 
-#     def _get_comp_id(self):
-#         comp_id = (self._comp_id + '_' + self._salt if self.config['use_salt']
-#                    else self._comp_id)
-#         return comp_id
-#
-#     def _get_voc_results_file_template(self):
-#         # VOCdevkit/results/VOC2007/Main/<comp_id>_det_test_aeroplane.txt
-#         filename = self._get_comp_id() + '_det_' + self._image_set + '_{:s}.txt'
-#         path = os.path.join(
-#             self._devkit_path,
-#             'results',
-#             'VOC' + self._year,
-#             'Main',
-#             filename)
-#         return path
-#
-#     def _write_voc_results_file(self, all_boxes):
-#         for cls_ind, cls in enumerate(self.classes):
-#             if cls == '__background__':
-#                 continue
-#             print('Writing {} VOC results file'.format(cls))
-#             filename = self._get_voc_results_file_template().format(cls)
-#             with open(filename, 'wt') as f:
-#                 for im_ind, index in enumerate(self.image_index):
-#                     dets = all_boxes[cls_ind][im_ind]
-#                     if dets == []:
-#                         continue
-#                     # the VOCdevkit expects 1-based indices
-#                     for k in range(dets.shape[0]):
-#                         f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
-#                                 format(index, dets[k, -1],
-#                                        dets[k, 0] + 1, dets[k, 1] + 1,
-#                                        dets[k, 2] + 1, dets[k, 3] + 1))
-#
-#     def _do_python_eval(self, output_dir='output'):
-#         annopath = self._devkit_path + '\\VOC' + self._year + '\\Annotations\\' + '{:s}.xml'
-#         imagesetfile = os.path.join(
-#             self._devkit_path,
-#             'VOC' + self._year,
-#             'ImageSets',
-#             'Main',
-#             self._image_set + '.txt')
-#         cachedir = os.path.join(self._devkit_path, 'annotations_cache')
-#         aps = []
-#         # The PASCAL VOC metric changed in 2010
-#         use_07_metric = True if int(self._year) < 2010 else False
-#         print('VOC07 metric? ' + ('Yes' if use_07_metric else 'No'))
-#         if not os.path.isdir(output_dir):
-#             os.mkdir(output_dir)
-#         for i, cls in enumerate(self._classes):
-#             if cls == '__background__':
-#                 continue
-#             filename = self._get_voc_results_file_template().format(cls)
-#             rec, prec, ap = voc_eval(
-#                 filename, annopath, imagesetfile, cls, cachedir, ovthresh=0.5,
-#                 use_07_metric=use_07_metric)
-#             aps += [ap]
-#             print(('AP for {} = {:.4f}'.format(cls, ap)))
-#             with open(os.path.join(output_dir, cls + '_pr.pkl'), 'wb') as f:
-#                 pickle.dump({'rec': rec, 'prec': prec, 'ap': ap}, f)
-#         print(('Mean AP = {:.4f}'.format(np.mean(aps))))
-#         print('~~~~~~~~')
-#         print('Results:')
-#         for ap in aps:
-#             print(('{:.3f}'.format(ap)))
-#         print(('{:.3f}'.format(np.mean(aps))))
-#         print('~~~~~~~~')
-#         print('')
-#         print('--------------------------------------------------------------')
-#         print('Results computed with the **unofficial** Python eval code.')
-#         print('Results should be very close to the official MATLAB eval code.')
-#         print('Recompute with `./tools/reval.py --matlab ...` for your paper.')
-#         print('-- Thanks, The Management')
-#         print('--------------------------------------------------------------')
-#
-#     def _do_matlab_eval(self, output_dir='output'):
-#         print('-----------------------------------------------------')
-#         print('Computing results with the official MATLAB eval code.')
-#         print('-----------------------------------------------------')
-#         path = os.path.join(cfg.FLAGS2["root_dir"], 'lib', 'datasets',
-#                             'VOCdevkit-matlab-wrapper')
-#         cmd = 'cd {} && '.format(path)
-#         cmd += '{:s} -nodisplay -nodesktop '.format('matlab')
-#         cmd += '-r "dbstop if error; '
-#         cmd += 'voc_eval(\'{:s}\',\'{:s}\',\'{:s}\',\'{:s}\'); quit;"' \
-#             .format(self._devkit_path, self._get_comp_id(),
-#                     self._image_set, output_dir)
-#         print(('Running:\n{}'.format(cmd)))
-#         status = subprocess.call(cmd, shell=True)
-#
-#     def evaluate_detections(self, all_boxes, output_dir):
-#         self._write_voc_results_file(all_boxes)
-#         self._do_python_eval(output_dir)
-#         if self.config['matlab_eval']:
-#             self._do_matlab_eval(output_dir)
-#         if self.config['cleanup']:
-#             for cls in self._classes:
-#                 if cls == '__background__':
-#                     continue
-#                 filename = self._get_voc_results_file_template().format(cls)
-#                 os.remove(filename)
-#
-#     def competition_mode(self, on):
-#         if on:
-#             self.config['use_salt'] = False
-#             self.config['cleanup'] = False
-#         else:
-#             self.config['use_salt'] = True
-#             self.config['cleanup'] = True
-#
-#
-# if __name__ == '__main__':
-#     from datasets.pascal_voc import pascal_voc
-#
-#     d = pascal_voc('trainval', '2007')
-#     res = d.roidb
-#     from IPython import embed;
-#
-#     embed()
+    def _get_comp_id(self):
+        comp_id = (self._comp_id + '_' + self._salt if self.config['use_salt']
+                   else self._comp_id)
+        return comp_id
+
+    def _get_voc_results_file_template(self):
+        # VOCdevkit/results/VOC2007/Main/<comp_id>_det_test_aeroplane.txt
+        filename = self._get_comp_id() + '_det_' + self._image_set + '_{:s}.txt'
+        path = os.path.join(
+            self._devkit_path,
+            'results',
+            'VOC' + self._year,
+            'Main',
+            filename)
+        return path
+
+    def _write_voc_results_file(self, all_boxes):
+        for cls_ind, cls in enumerate(self.classes):
+            if cls == '__background__':
+                continue
+            print('Writing {} VOC results file'.format(cls))
+            filename = self._get_voc_results_file_template().format(cls)
+            with open(filename, 'wt') as f:
+                for im_ind, index in enumerate(self.image_index):
+                    dets = all_boxes[cls_ind][im_ind]
+                    if dets == []:
+                        continue
+                    # the VOCdevkit expects 1-based indices
+                    for k in range(dets.shape[0]):
+                        f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
+                                format(index, dets[k, -1],
+                                       dets[k, 0] + 1, dets[k, 1] + 1,
+                                       dets[k, 2] + 1, dets[k, 3] + 1))
+
+    def _do_python_eval(self, output_dir='output'):
+        annopath = self._devkit_path + '\\VOC' + self._year + '\\Annotations\\' + '{:s}.xml'
+        imagesetfile = os.path.join(
+            self._devkit_path,
+            'VOC' + self._year,
+            'ImageSets',
+            'Main',
+            self._image_set + '.txt')
+        cachedir = os.path.join(self._devkit_path, 'annotations_cache')
+        aps = []
+        # The PASCAL VOC metric changed in 2010
+        use_07_metric = True if int(self._year) < 2010 else False
+        print('VOC07 metric? ' + ('Yes' if use_07_metric else 'No'))
+        if not os.path.isdir(output_dir):
+            os.mkdir(output_dir)
+        for i, cls in enumerate(self._classes):
+            if cls == '__background__':
+                continue
+            filename = self._get_voc_results_file_template().format(cls)
+            rec, prec, ap = voc_eval(
+                filename, annopath, imagesetfile, cls, cachedir, ovthresh=0.5,
+                use_07_metric=use_07_metric)
+            aps += [ap]
+            print(('AP for {} = {:.4f}'.format(cls, ap)))
+            with open(os.path.join(output_dir, cls + '_pr.pkl'), 'wb') as f:
+                pickle.dump({'rec': rec, 'prec': prec, 'ap': ap}, f)
+        print(('Mean AP = {:.4f}'.format(np.mean(aps))))
+        print('~~~~~~~~')
+        print('Results:')
+        for ap in aps:
+            print(('{:.3f}'.format(ap)))
+        print(('{:.3f}'.format(np.mean(aps))))
+        print('~~~~~~~~')
+        print('')
+        print('--------------------------------------------------------------')
+        print('Results computed with the **unofficial** Python eval code.')
+        print('Results should be very close to the official MATLAB eval code.')
+        print('Recompute with `./tools/reval.py --matlab ...` for your paper.')
+        print('-- Thanks, The Management')
+        print('--------------------------------------------------------------')
+
+    def _do_matlab_eval(self, output_dir='output'):
+        print('-----------------------------------------------------')
+        print('Computing results with the official MATLAB eval code.')
+        print('-----------------------------------------------------')
+        path = os.path.join(cfg.FLAGS2["root_dir"], 'lib', 'datasets',
+                            'VOCdevkit-matlab-wrapper')
+        cmd = 'cd {} && '.format(path)
+        cmd += '{:s} -nodisplay -nodesktop '.format('matlab')
+        cmd += '-r "dbstop if error; '
+        cmd += 'voc_eval(\'{:s}\',\'{:s}\',\'{:s}\',\'{:s}\'); quit;"' \
+            .format(self._devkit_path, self._get_comp_id(),
+                    self._image_set, output_dir)
+        print(('Running:\n{}'.format(cmd)))
+        status = subprocess.call(cmd, shell=True)
+
+    def evaluate_detections(self, all_boxes, output_dir):
+        self._write_voc_results_file(all_boxes)
+        self._do_python_eval(output_dir)
+        if self.config['matlab_eval']:
+            self._do_matlab_eval(output_dir)
+        if self.config['cleanup']:
+            for cls in self._classes:
+                if cls == '__background__':
+                    continue
+                filename = self._get_voc_results_file_template().format(cls)
+                os.remove(filename)
+
+    def competition_mode(self, on):
+        if on:
+            self.config['use_salt'] = False
+            self.config['cleanup'] = False
+        else:
+            self.config['use_salt'] = True
+            self.config['cleanup'] = True
+
+
+if __name__ == '__main__':
+    from datasets.pascal_voc import pascal_voc
+
+    d = pascal_voc('trainval', '2007')
+    res = d.roidb
+    from IPython import embed;
+
+    embed()
---
+++ val.py
@@ -0,0 +1,85 @@
+# --------------------------------------------------------
+# Tensorflow Faster R-CNN
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Hangyan Jiang, based on code from Ross Girshick
+# --------------------------------------------------------
+
+
+"""
+Demo script showing detections in sample images.
+
+See README.md for installation instructions before running.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from lib.config import config as cfg
+from lib.utils.nms_wrapper import nms
+from lib.utils.test import im_detect
+#from nets.resnet_v1 import resnetv1
+from lib.nets.vgg16 import vgg16
+from lib.utils.timer import Timer
+from train import combined_roidb
+
+CLASSES = ('__background__',
+           'tampered')
+
+# PLEASE specify weight files dir for vgg16
+NETS = {'vgg16': ('vgg16_faster_rcnn_iter_30000.ckpt',), 'res101': ('res101_faster_rcnn_iter_110000.ckpt',)}
+DATASETS = {'pascal_voc': ('voc_2007_trainval',), 'pascal_voc_0712': ('voc_2007_trainval+voc_2012_trainval',)}
+
+def parse_args():
+    """Parse input arguments."""
+    parser = argparse.ArgumentParser(description='Tensorflow Faster R-CNN demo')
+    parser.add_argument('--net', dest='demo_net', help='Network to use [vgg16 res101]',
+                        choices=NETS.keys(), default='vgg16')
+    parser.add_argument('--dataset', dest='dataset', help='Trained dataset [pascal_voc pascal_voc_0712]',
+                        choices=DATASETS.keys(), default='pascal_voc_0712')
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    # model path
+    demonet = args.demo_net
+    dataset = args.dataset
+    tfmodel = os.path.join('default', 'DIY_dataset', 'default', NETS[demonet][0])
+
+    if not os.path.isfile(tfmodel + '.meta'):
+        print(tfmodel)
+        raise IOError(('{:s} not found.\nDid you download the proper networks from '
+                       'our server and place them properly?').format(tfmodel + '.meta'))
+
+    # set config
+    tfconfig = tf.ConfigProto(allow_soft_placement=True)
+    tfconfig.gpu_options.allow_growth = True
+
+    # init session
+    sess = tf.Session(config=tfconfig)
+    # load network
+    if demonet == 'vgg16':
+        net = vgg16(batch_size=1)
+    # elif demonet == 'res101':
+        # net = resnetv1(batch_size=1, num_layers=101)
+    else:
+        raise NotImplementedError
+    net.create_architecture(sess, "TEST", 2,
+                            tag='default', anchor_scales=[8, 16, 32])
+    saver = tf.train.Saver()
+    saver.restore(sess, tfmodel)
+
+    print('Loaded network {:s}'.format(tfmodel))
+
+    imdb, _ = combined_roidb("DIY_dataset")
+
+    test_net(sess, net, imdb, weights_filename='output')
