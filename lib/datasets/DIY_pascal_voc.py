b2c773943c8e37aa1e0be13f0bf80e9f6137354e
DESKTOP-M7BS054\nlandy <nlandy8@gmail.com>
2019-05-14 21:10:08 -0700

    use 07 metric

--- lib/datasets/DIY_pascal_voc.py
+++ lib/datasets/DIY_pascal_voc.py
@@ -215,7 +215,7 @@ class DIY_pascal_voc(imdb):
             if cls == '__background__':
                 continue
             filename = self._get_voc_results_file_template().format(cls)
-            print(filename)
+            print(use_07_metric)
             rec, prec, ap = voc_eval(
                 filename, annopath, imagesetfile, cls, cachedir, ovthresh=0.5,
                 use_07_metric=use_07_metric)
