7e7bda4566500360bbaf6e73775028f69a7f695c
DESKTOP-M7BS054\nlandy <nlandy8@gmail.com>
2019-05-15 01:57:37 -0700

    self

--- train.py
+++ train.py
@@ -68,7 +68,7 @@ class Train:
             raise NotImplementedError
 
         self.imdb, self.roidb = combined_roidb("DIY_dataset")
-        print((len(imdb), len(roidb)))
+        print((len(self.imdb), len(self.roidb)))
 
         self.data_layer = RoIDataLayer(self.roidb, self.imdb.num_classes)
         self.output_dir = cfg.get_output_dir(self.imdb, 'default')
