1fe0c994fd34690493872b7119f3392a9a1214bb
DESKTOP-M7BS054\nlandy <nlandy8@gmail.com>
2019-05-14 21:18:44 -0700

    0 for diff

--- lib/datasets/voc_eval.py
+++ lib/datasets/voc_eval.py
@@ -134,7 +134,7 @@ def voc_eval(detpath,
     for imagename in imagenames:
         R = [obj for obj in recs[imagename] if obj['name'] == classname]
         bbox = np.array([x['bbox'] for x in R])
-        difficult = np.array([1 for x in R]).astype(np.bool)
+        difficult = np.array([0 for x in R]).astype(np.bool)
         det = [False] * len(R)
         npos = npos + sum(~difficult)
         class_recs[imagename] = {'bbox': bbox,
