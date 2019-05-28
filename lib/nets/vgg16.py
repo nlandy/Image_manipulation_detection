ea49d5fd406aa7d840b70ab3d1e1b856d8074608
DESKTOP-M7BS054\nlandy <nlandy8@gmail.com>
2019-05-28 13:19:51 -0700

    remove layers for second channnel

--- lib/nets/vgg16.py
+++ lib/nets/vgg16.py
@@ -57,7 +57,8 @@ class vgg16(Network):
             net = self.build_head(is_training)
 
             # Build Noise stream head
-            net2 = self.build_head_forNoise(is_training, initializer, initializer_srm)
+            net2 = None
+            #net2 = self.build_head_forNoise(is_training, initializer, initializer_srm)
 
             # Build rpn
             rpn_cls_prob, rpn_bbox_pred, rpn_cls_score, rpn_cls_score_reshape = self.build_rpn(net, is_training, initializer)
@@ -292,9 +293,7 @@ class vgg16(Network):
         # Compact Bilinear Pooling
         #cbp = compact_bilinear_pooling_layer(pool5, pool5_forNoise, 512)
         cbp_flat = slim.flatten(pool5, scope='cbp_flatten')
-        #print(pool5.get_shape())
-        #print(pool5_forNoise.get_shape())
-        #print(cbp_flat.get_shape())
+
         # Fully connected layers
         # fc6 = slim.fully_connected(pool5_flat, 4096, scope='bbox_fc6')
         fc6_cbp = slim.fully_connected(cbp_flat, 4096, scope='fc6')
