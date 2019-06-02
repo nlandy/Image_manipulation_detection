e5caf8a2b574b00e68b491353130a5a790b50785
DESKTOP-M7BS054\nlandy <nlandy8@gmail.com>
2019-06-02 03:49:32 -0700

    change to columba

--- val.py
+++ val.py
@@ -41,7 +41,7 @@ def parse_args():
     """Parse input arguments."""
     parser = argparse.ArgumentParser(description='Tensorflow Faster R-CNN demo')
     parser.add_argument('--net', dest='demo_net', help='Network to use [vgg16 res50]',
-                        choices=NETS.keys(), default='res50')
+                        choices=NETS.keys(), default='vgg16')
     parser.add_argument('--dataset', dest='dataset', help='Trained dataset [pascal_voc pascal_voc_0712]',
                         choices=DATASETS.keys(), default='pascal_voc_0712')
     args = parser.parse_args()
@@ -54,7 +54,7 @@ if __name__ == '__main__':
     # model path
     demonet = args.demo_net
     dataset = args.dataset
-    tfmodel = os.path.join('default', 'DIY_dataset', 'default', NETS[demonet][0])
+    tfmodel = os.path.join('default', 'Columbia', 'default', NETS[demonet][0])
 
     if not os.path.isfile(tfmodel + '.meta'):
         print(tfmodel)
@@ -81,6 +81,6 @@ if __name__ == '__main__':
 
     print('Loaded network {:s}'.format(tfmodel))
 
-    imdb, _ = combined_roidb("DIY_dataset")
+    imdb, _ = combined_roidb("Columbia")
 
     test_net(sess, net, imdb, weights_filename='output')
