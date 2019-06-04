e5fcf67892050c0aa52f3a9b7e2a26f05fc906c6
DESKTOP-M7BS054\nlandy <nlandy8@gmail.com>
2019-06-04 15:34:28 -0700

    try printing vars

--- vis_filters.py
+++ vis_filters.py
@@ -74,15 +74,18 @@ def get_model():
         net = resnetv1(batch_size=1, num_layers=50)
     else:
         raise NotImplementedError
-    net.create_architecture(sess, "TEST", 2,
-                            tag='default', anchor_scales=[8, 16, 32])
+    #net.create_architecture(sess, "TEST", 2, tag='default', anchor_scales=[8, 16, 32])
 
-    srm2 = tf.get_variable("srm2", [5, 5, 3, 3], trainable=False)
+    #srm2 = tf.get_variable("srm2", [5, 5, 3, 3], trainable=False)
 
     saver = tf.train.Saver()
     saver.restore(sess, tfmodel)
 
-    print(srm2.eval())
+    variables = tf.global_variables()
+    for i in variables:
+        print(i)
+
+    #print(srm2.eval())
 
     #print('Loaded network {:s}'.format(tfmodel))
 
