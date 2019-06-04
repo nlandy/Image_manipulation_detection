aefbf4799b722e7422af9f21c89afe2463f7fc2f
DESKTOP-M7BS054\nlandy <nlandy8@gmail.com>
2019-06-04 15:45:26 -0700

    try again

--- vis_filters.py
+++ vis_filters.py
@@ -76,7 +76,7 @@ def get_model():
         raise NotImplementedError
     #net.create_architecture(sess, "TEST", 2, tag='default', anchor_scales=[8, 16, 32])
 
-    srm2 = tf.get_variable('vgg_16/srm2/weights:0', [5, 5, 3, 3], trainable=False)
+    srm2 = tf.get_variable('vgg_16/srm2/weights', [5, 5, 3, 3], trainable=False)
 
     saver = tf.train.Saver()
     saver.restore(sess, tfmodel)
