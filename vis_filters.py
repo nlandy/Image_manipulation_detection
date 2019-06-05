54135c3d4e97b82c318071812eebf57a960c58a2
DESKTOP-M7BS054\nlandy <nlandy8@gmail.com>
2019-06-04 18:54:50 -0700

    update to res50

--- vis_filters.py
+++ vis_filters.py
@@ -43,7 +43,7 @@ def parse_args():
     """Parse input arguments."""
     parser = argparse.ArgumentParser(description='Tensorflow Faster R-CNN demo')
     parser.add_argument('--net', dest='demo_net', help='Network to use [vgg16 res50]',
-                        choices=NETS.keys(), default='vgg16')
+                        choices=NETS.keys(), default='res50')
     parser.add_argument('--dataset', dest='dataset', help='Trained dataset [pascal_voc pascal_voc_0712]',
                         choices=DATASETS.keys(), default='pascal_voc_0712')
     args = parser.parse_args()
